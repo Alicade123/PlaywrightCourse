5d17786dcf104ec293135f2fac361559f1485770
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-27 17:28:38 +0200

    running the first test using playright

---
+++ src/test/java/part1/LunchBrowser.java
@@ -0,0 +1,29 @@
+package part1;
+
+import com.microsoft.playwright.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LunchBrowser {
+
+    public static void main(String[] args) {
+        Playwright playwright =Playwright.create();
+//        Browser Options: webkit, firefox, chromium
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page= browser.newPage();
+        page.navigate("https://ecommerce-playground.lambdatest.io/");
+        Locator myAccountElement = page.locator("//a[contains(.,'My account')][@role='button']");
+        myAccountElement.hover();
+        page.locator("//a[contains(.,'Login')]").click();
+        assertThat(page).hasTitle("Account Login");
+        page.getByPlaceholder("E-Mail Address").type("koushik350@gmail.com");
+        page.getByPlaceholder("Password").type("Pass123$");
+        page.locator("//input[@value='Login']").click();
+        assertThat(page).hasTitle("My Account");
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
