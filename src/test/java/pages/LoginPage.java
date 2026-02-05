1002029b9d58bfe5cff8552a4c081c216862ea5b
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-02-05 15:44:39 +0200

    Remove old page and example files after restructuring

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,14 +3,21 @@ package pages;
 import com.microsoft.playwright.Page;
 
 public class LoginPage {
-    private Page page = null;
-    public LoginPage(Page page) {
+    private final Page page;
+    private final String loginUsernameField = "input[name='username']";
+    private final String loginPasswordField = "input[name='password']";
+    private final String loginBtn = "button[type='submit']";
+    public LoginPage(Page page){
         this.page = page;
     }
-
-    public void loginAsUser(String email, String password) {
-        page.getByLabel("E-Mail Address").fill(email);
-        page.getByLabel("Password").fill(password);
-        page.locator("//input[@value='Login']").click();
+    public void addUsername(String username){
+        page.fill(loginUsernameField, username);
     }
+    public void addPassword(String passcode){
+        page.fill(loginPasswordField, passcode);
+    }
+    public void clickLoginButton(){
+        page.click(loginBtn);
+    }
+
 }
